f4007f3eaaa52ffc6d9369cb9ad605d0587b0662
Christopher Lane Hinson <lane@downstairspeople.org>
2016-07-10 23:48:29 -0400

    Use relative paths when adding.

--- src/add.js
+++ src/add.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const fs = require('fs-promise');
+const path = require('path');
 const Resource = require('./Resource');
 
 module.exports = function(text_filepath, index_filepath) {
   return fs.access(text_filepath, fs.R_OK)
     .then(() => Resource.resolve(index_filepath))
     .then(resource => resource.json().then(json => {
-      json.sections.push(text_filepath); //relative
+      json.sections.push(path.relative(index_filepath,text_filepath));
       return resource.write(JSON.stringify(json));
     }));
 };
